691778aadd80cc5d0609982c249f6b12f075fff8
Sami <hellosami@protonmail.com>
2023-07-31 11:46:22 +0200

    Refactor: add setup method in testclass

--- src/test/java/org/example/Tests.java
+++ src/test/java/org/example/Tests.java
@@ -6,26 +6,27 @@ import org.junit.jupiter.api.Test;
 
 public class Tests {
 
+    Table sut;
+
+    @BeforeEach
+    public void setup() {
+        sut = new Table(4, 9, 2, 7);;
+    }
+
     @Test
     public void createTableWithWidthAndHeight() {
-        Table sut = new Table(4, 9, 2, 7);
-
         Assertions.assertEquals(4, sut.getWidth());
         Assertions.assertEquals(9, sut.getHeight());
     }
 
     @Test
     public void createTableWithPosition() {
-        Table sut = new Table(4, 9, 2, 7);
-
         Assertions.assertEquals(2, sut.getPositionX());
         Assertions.assertEquals(7, sut.getPositionY());
     }
 
     @Test
     public void createTableWithDirectionNorthAsDefault() {
-        Table sut = new Table(4, 9, 2, 7);
-
         Assertions.assertEquals(Direction.NORTH, sut.getDirection());
     }
 
