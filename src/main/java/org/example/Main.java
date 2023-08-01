61177e84b0601a386a97abdddd05476badc2fe03
Sami <hellosami@protonmail.com>
2023-08-01 13:20:21 +0200

    Setup scanner and simulation in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in);
+        String inputSize = scan.nextLine();
+        String inputPosition = scan.nextLine();
+        String inputCommands = scan.nextLine();
+        String[] commands = inputCommands.split(",");
+
+        int width = Integer.parseInt(inputSize.split(",")[0]);
+        int height = Integer.parseInt(inputSize.split(",")[1]);
+        int x = Integer.parseInt(inputPosition.split(",")[0]);
+        int y = Integer.parseInt(inputPosition.split(",")[1]);
+
+        Table table = new Table(width, height, x, y);
+        for (String com : commands) {
+            int value = Integer.parseInt(com);
+            table.movePosition(value);
+        }
+        scan.close();
     }
 }
