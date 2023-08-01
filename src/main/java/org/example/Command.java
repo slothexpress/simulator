50228c2fec13e42deb6c3171abd11d0ab83684ce
Sami <hellosami@protonmail.com>
2023-08-01 13:18:51 +0200

    Add integer values to enum constants

--- src/main/java/org/example/Command.java
+++ src/main/java/org/example/Command.java
@@ -1,9 +1,16 @@
 package org.example;
 
 public enum Command {
-    QUIT,
-    FORWARD,
-    BACKWARD,
-    ROTATE_CLOCKWISE,
-    ROTATE_COUNTERCLOCKWISE
+    QUIT(0),
+    FORWARD(1),
+    BACKWARD(2),
+    ROTATE_CLOCKWISE(3),
+    ROTATE_COUNTERCLOCKWISE(4);
+
+    private final int value;
+
+    Command(int value) {
+        this.value = value;
+    }
+
 }
